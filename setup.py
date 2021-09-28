349343bdc1779a0a32a2077917c2e04c118558c9
Sushant Sur <sushant.sur23@gmail.com>
2021-09-28 22:48:20 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "src", 
+    version="0.0.1",
+    author ="sushantsur23",
+    description="A small package for dvc pipeline demo",
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/sushantsur23/dvc_level1.git",
+    author_email="sushant.sur23@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires= [
+        "dvc",
+        "pandas",
+        "scikit-learn",
+        "numpy"
+    ]
+)
