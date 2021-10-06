af0c44e1215e716fb2a67e585265e72486b9c642
Sushant Sur <sushant.sur23@gmail.com>
2021-10-07 00:18:33 +0530

    first

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -12,3 +12,7 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path)
         print(f"directory is created at {dir_path}")
+
+def save_local_df(data, data_path, index_status=False):
+    data.to_csv(data_path, index=index_status)
+    print(f"data is asved at {data_path}")
